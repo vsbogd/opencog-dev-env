e0ddf389e0f70d8707d9e4525b7552d10beaaa79
Vitaly Bogdanov <vsbogd@gmail.com>
2019-03-12 18:27:48 +0300

    Fix path and ids for work workstation

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-SOURCE_DIR=${HOME}/temp/agi
-DOCKER_HOME=/home/opencog
-POSTGRES_CONTAINER="opencog-postgres"
-
+SOURCE_DIR=${HOME}/projects/opencog
+DOCKER_HOME=/home/vital
+POSTGRES_CONTAINER="vigilant_galileo"
 docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
 	-u `id -u`:`id -g` \
     --mount="type=bind,src=${SOURCE_DIR},dst=${DOCKER_HOME}/projects" \
